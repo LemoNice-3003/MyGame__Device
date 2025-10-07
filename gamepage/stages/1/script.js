ef77bc7b3c11183db2292e122aa861595b512656
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-10-07 14:40:34 +0900

    Update script.js
    10/7
    変更点
    サイト読み込み時に、最初に押されたキーが↑↓意外だった場合に数値がおかしくなってしまう欠陥を修正（12行目に入力制限を増やした
    
    改善点
    とっととゲームのロジックに取り組む

--- gamepage/stages/1/script.js
+++ gamepage/stages/1/script.js
@@ -9,16 +9,20 @@ document.getElementById("moveTile").innerHTML = volumeNum;
 document.addEventListener("keydown", function(event) {
     volumeStr = $moveTile.css('--volume').trim();
     volumeNum = parseFloat(volumeStr);
-    if (event.key == "ArrowDown" && volumeNum > 0) {
-        newVolume = volumeNum - 1;
+    if (event.key == "ArrowDown" || event.key == "ArrowUp") {
+        if (event.key == "ArrowDown" && volumeNum > 0) {
+            newVolume = volumeNum - 1;
+        }
+        if (event.key == "ArrowUp" && volumeNum < 60) {
+            newVolume = volumeNum + 1;
+        }
+        
+        var displayVolume = Math.floor(newVolume * (100 / 60));
+        document.getElementById("moveTile").innerHTML = displayVolume;
+        $(function changeVolume() {
+            $moveTile.css('--volume', Math.floor(newVolume) + '%');
+        });
     }
-    if (event.key == "ArrowUp" && volumeNum < 60) {
-        newVolume = volumeNum + 1;
+    else {
     }
-
-    var displayVolume = Math.floor(newVolume * (100 / 60));
-    document.getElementById("moveTile").innerHTML = displayVolume;
-    $(function changeVolume() {
-        $moveTile.css('--volume', Math.floor(newVolume) + '%');
-    });
 });
