5dd2616f883a7844eb757f918d665b5753473d87
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-10-21 13:14:32 +0900

    Update script.js
    10/21
    変更点
    前回の更新でステージ１しか実装してなかったロジックをすべてに反映
    
    改善点
    できれば、画像が表示される際のアニメーションも実装したい

--- gamepage/script.js
+++ gamepage/script.js
@@ -27,90 +27,138 @@ $(document).ready(function() {
             stage_1.removeClass("active");
         }
     });
-    
-    $('#st1-2')
-        .bind('click', function(){
+    $('#st1-2').on({
+        click : function(){
             stage_2.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/2.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st1-2')) {
             stage_2.removeClass("active");
         }
     });
-
-    $('#st1-3')
-        .bind('click', function(){
+    $('#st1-3').on({
+        click : function(){
             stage_3.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/3.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st1-3')) {
             stage_3.removeClass("active");
         }
     });
-
-    $('#st2-1')
-        .bind('click', function(){
+    $('#st2-1').on({
+        click : function(){
             stage_4.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/4.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st2-1')) {
             stage_4.removeClass("active");
         }
     });
-
-    $('#st2-2')
-        .bind('click', function(){
+    $('#st2-2').on({
+        click : function(){
             stage_5.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/5.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st2-2')) {
             stage_5.removeClass("active");
         }
     });
-
-    $('#st2-3')
-        .bind('click', function(){
+    $('#st2-3').on({
+        click : function(){
             stage_6.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/6.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st2-3')) {
             stage_6.removeClass("active");
         }
     });
-
-    $('#st3-1')
-        .bind('click', function(){
+    $('#st3-1').on({
+        click : function(){
             stage_7.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/7.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st3-1')) {
             stage_7.removeClass("active");
         }
     });
-
-    $('#st3-2')
-        .bind('click', function(){
+    $('#st3-2').on({
+        click : function(){
             stage_8.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/8.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st3-2')) {
             stage_8.removeClass("active");
         }
     });
-
-    $('#st3-3')
-        .bind('click', function(){
+    $('#st3-3').on({
+        click : function(){
             stage_9.addClass("active");
+        },
+        mouseenter : function() {
+            preview.css("background-image", "url(../images/stage_thumbnail/9.jpg)");
+        },
+        mouseleave : function() {
+            preview.css("background-image", "none");
+        }
     });
     $(document).on('click', function(e) {
-        
+        // クリックされた要素が input 以外なら active クラスを削除
         if (!$(e.target).is('#st3-3')) {
             stage_9.removeClass("active");
         }
