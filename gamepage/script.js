a62a5bc78d6158984dc1ccdb509b5276503363d8
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-12-16 15:11:14 +0900

    Update script.js
    12/16
    変更点
    ログイン関係
    
    改善点
    反映がうまくできない

--- gamepage/script.js
+++ gamepage/script.js
@@ -1,5 +1,3 @@
-const userName = sessionStorage.getItem('name');
-const valueNum = Number(sessionStorage.getItem('progress'));
 $(document).ready(function() {
     // ここで$を使用できる
     const st1_1 = $("#st1-1");
@@ -217,7 +215,9 @@ $(document).ready(function() {
 const root = document.documentElement;
 
 const customElement = document.querySelector('#header_h1');
-const textLength = parseInt(userName.length) + 7;
+try {
+    textLength = parseInt(userName.length) + 7;
+} catch { alert("miss"); }
 customElement.style.setProperty('--nameLength', textLength + "em");
 
 document.addEventListener('wheel', function(event) {
@@ -226,17 +226,6 @@ document.addEventListener('wheel', function(event) {
     }
 }, { passive: false });
 
-
-const hamburgerMenu = document.querySelector('#js-hamburger');
-const hamburgerItems = document.querySelector('#js-items');
-const container = document.querySelector('#js-container');
-
-// ハンバーガーメニューがクリックされたら
-hamburgerMenu.addEventListener('click', function() {
-    hamburgerMenu.classList.toggle('active');
-    hamburgerItems.classList.toggle('active');
-    container.classList.toggle('active');
-});
 
 
 
