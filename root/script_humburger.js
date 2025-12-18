cac2a2d5f7ead60bfce345d1729fbb50c30904ae
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-12-18 20:18:34 +0900

    Update script_humburger.js
    12/18
    変更点
    ・表示の変更方法を、displayからopacityとpointereventsで制御する方式にした
    ・onload()とその後の処理の順番を逆にした
    
    改善点
    ログイン、ログアウトの処理もこっちに入れるかも

--- root/script_humburger.js
+++ root/script_humburger.js
@@ -1,16 +1,24 @@
 nowUserName.style.setProperty('--nameLength', textLength + "em");
 function onload() {
-    if((loginFlag == false) || (typeof sessionStorage === "undefined") || (sessionStorage.length === 0)){ // 初期状態または"0"だった場合
-        alert("アカウントなし");
-        $("#nowUserName").text("");
-        loginNow.style.display = "flex";
-        loginButton.style.display = "none";
-    }
-    else {
-        alert("アカウント認識");
+    if(loginFlag == "true") {
+        // alert("アカウント認識");
         $("#nowUserName").text(userName);
-        loginNow.style.display = "none";
-        loginButton.style.display = "block";
+
+        loginNow.style.opacity = 1;
+        loginNow.style.pointerEvents = 'auto';
+
+        loginButton.style.opacity = 0;
+        loginButton.style.pointerEvents = 'none';
+    }
+    else if((loginFlag == "false") || (typeof sessionStorage === "undefined") || (sessionStorage.length === 0)){ // 初期状態または"0"だった場合
+        // alert("アカウントなし");
+        $("#nowUserName").text("");
+
+        loginNow.style.opacity = 0;
+        loginNow.style.pointerEvents = 'none';
+
+        loginButton.style.opacity = 1;
+        loginButton.style.pointerEvents = 'auto';
     }
 }
 
@@ -22,9 +30,13 @@ hamburgerMenu.addEventListener('click', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    onload();
     try {
         textLength = parseInt(userName.length) + 2;
-    } catch { alert("miss"); }
+        sessionStorage.setItem('loginFlag', true);
+    } catch {
+        // alert(`名前：${textLength}文字`);
+        sessionStorage.setItem('loginFlag', false);
+    }
     nowUserName.style.setProperty('--nameLength', textLength + "em");
+    onload();
 });
