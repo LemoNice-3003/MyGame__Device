2be30b77f4d6dbb1ec32b70f80e723652a599cef
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-12-23 14:58:39 +0900

    Update script.js
    12/23
    変更点
    ・時計を完成させた
    ・timeZone関係の試験機能として、真ん中のbackground-colorが虹色で変化するようにしている
    
    改善点
    次回更新時には９割完成を目指す

--- gamepage/stages/3/script.js
+++ gamepage/stages/3/script.js
@@ -1,53 +1,92 @@
-let secondTimer;
-let minuteTimer;
-let hourTimer;
-secondTimer = setInterval(secondClock, 1000);
-minuteTimer = setInterval(minuteClock, 60000);
-hourTimer = setInterval(hourClock, 3600000);
+let clockTimer;
+clockTimer = setInterval(updateClock, 1000);
 
 const secondDial = document.getElementById("second");
 const minuteDial = document.getElementById("minute");
 const hourDial = document.getElementById("hour");
-let secondAngle = 0;
-let minuteAngle = 0;
-let hourAngle = 0;
 
+let lastSecondDeg = 0;
+let lastMinuteDeg = 0;
+let lastHourDeg = 0;
+let secondRotation = 0;
+let minuteRotation = 0;
+let hourRotation = 0;
+
+const timeZone = document.getElementById("timeZone");
+var soraColor =  {
+    1:'#FF0000',
+    2:'#FF7F00',
+    3:'#FFFF00',
+    4:'#00FF00',
+    5:'#0000FF',
+    6:'#4B0082',
+    7:'#9400D3'
+};
+let colorCount = 1;
 
 function onload() {
     if(nowProgress >= 4) {
         checkClearIcon();
     }
 
-    let now = new Date();
-    let hour24 = now.getHours();
-    let hour12 = hour24 % 12 || 12;
-    console.log(hour12);
-    secondAngle = parseInt(now.getSeconds()) * 6;
-    minuteAngle = parseInt(now.getMinutes()) * 6;
-    hourAngle = parseInt(hour12) * 30;
-    secondDial.style.transform = `rotate(${secondAngle}deg)`;
-    minuteDial.style.transform = `rotate(${minuteAngle}deg)`;
-    hourDial.style.transform = `rotate(${hourAngle}deg)`;
+    updateClock();
 }
 
-function secondClock() {
-    let now = new Date();
-    secondAngle += 6;
+function updateClock() {
+    const now = new Date();
+
+    const ms = now.getMilliseconds();
+
+    const second = now.getSeconds() + ms / 1000;
+    const secondCurrentDeg = second * 6;
+    lastSecondDeg = checkOverFllow("second", secondCurrentDeg, lastSecondDeg);
+    
+    const minute = now.getMinutes() + second / 60;
+    const minuteCurrentDeg = minute * 6;
+    lastMinuteDeg = checkOverFllow("minute", minuteCurrentDeg, lastMinuteDeg);
+    
+    const hour = now.getHours() % 12 + minute / 60;
+    const hourCurrentDeg = hour * 30;
+    lastHourDeg = checkOverFllow("hour", hourCurrentDeg, lastHourDeg);
+    
+
+    const secondAngle = secondCurrentDeg + secondRotation;
+    const minuteAngle = minuteCurrentDeg + minuteRotation;
+    const hourAngle = hourCurrentDeg + hourRotation;
+
+
     secondDial.style.transform = `rotate(${secondAngle}deg)`;
-    console.log(now.toLocaleTimeString('ja-JP', {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-    }));
-}
-function minuteClock() {
-    minuteAngle += 6;
     minuteDial.style.transform = `rotate(${minuteAngle}deg)`;
-    minuteDial.style.transition = `transform 60s linear`;
-}
-function hourClock() {
-    hourAngle += 30;
     hourDial.style.transform = `rotate(${hourAngle}deg)`;
-    hourDial.style.transition = `transform 3600s linear`;
+
+    if(colorCount > 7) {
+        colorCount = 1;
+    }
+    timeZone.style.setProperty('--sora-color', soraColor[colorCount]);
+    colorCount++;
+}
+
+function checkOverFllow(type, current, deg) {
+    switch(type) {
+        case "second":
+            if(current < deg) {
+                secondRotation += 360;
+            }
+            return current;
+            
+        case "minute":
+            if(current < deg) {
+                minuteRotation += 360;
+            }
+            return current;
+        
+        case "hour":
+            if(current < deg) {
+                hourRotation += 360;
+            }
+            return current;
+
+        default:
+            break;
+    }
 }
