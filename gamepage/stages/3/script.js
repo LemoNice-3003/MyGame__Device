52d5266f13997a60a8452f4232d28090ba8b4b01
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-12-25 00:43:18 +0900

    Update script.js
    12/25
    変更点
    ・soraColorの色を決めた
    ・辞書の連想方式を利用して、取得した時間を辞書のkeyとして色を適応することで、1時間ごとに色が変化する構造
    ・クリアアイコンの試験用にコードを追加（l.46
    
    改善点
    ・何故かクリアアイコンが変化しない（パーティクルは出るし、ステージ１では普通に動作してるからなおさら謎
    ・あと実装する機能は、時間帯に応じて変化させるクリアアイコンを変えて、進捗の保存方法をどうするか決めて実装する

--- gamepage/stages/3/script.js
+++ gamepage/stages/3/script.js
@@ -14,19 +14,36 @@ let hourRotation = 0;
 
 const timeZone = document.getElementById("timeZone");
 var soraColor =  {
-    1:'#FF0000',
-    2:'#FF7F00',
-    3:'#FFFF00',
-    4:'#00FF00',
-    5:'#0000FF',
-    6:'#4B0082',
-    7:'#9400D3'
+    0:'#01002a',
+    1:'#0b0052',
+    2:'#1f136f',
+    3:'#303990',
+    4:'#5358a1',
+    5:'#7dadde',
+    6:'#b2e0f7',
+    7:'#aedef5',
+    8:'#95d4f5',
+    9:'#7ac9f2',
+    10:'#5cc1ed',
+    11:'#42bced',
+    12:'#1eb7ed',
+    13:'#56c0ef',
+    14:'#92d0dd',
+    15:'#beca90',
+    16:'#ddad51',
+    17:'#ed7002',
+    18:'#b0534b',
+    19:'#6c2f6a',
+    20:'#2e238d',
+    21:'#1e136f',
+    22:'#130769',
+    23:'#000046'
 };
 let colorCount = 1;
 
 function onload() {
     if(nowProgress >= 4) {
-        checkClearIcon();
+        checkClearIcon(checkbox_2_0, "30vw", "50vh");
     }
 
     updateClock();
@@ -39,15 +56,15 @@ function updateClock() {
 
     const second = now.getSeconds() + ms / 1000;
     const secondCurrentDeg = second * 6;
-    lastSecondDeg = checkOverFllow("second", secondCurrentDeg, lastSecondDeg);
+    lastSecondDeg = checkOverflow("second", secondCurrentDeg, lastSecondDeg);
     
     const minute = now.getMinutes() + second / 60;
     const minuteCurrentDeg = minute * 6;
-    lastMinuteDeg = checkOverFllow("minute", minuteCurrentDeg, lastMinuteDeg);
+    lastMinuteDeg = checkOverflow("minute", minuteCurrentDeg, lastMinuteDeg);
     
     const hour = now.getHours() % 12 + minute / 60;
     const hourCurrentDeg = hour * 30;
-    lastHourDeg = checkOverFllow("hour", hourCurrentDeg, lastHourDeg);
+    lastHourDeg = checkOverflow("hour", hourCurrentDeg, lastHourDeg);
     
 
     const secondAngle = secondCurrentDeg + secondRotation;
@@ -59,14 +76,15 @@ function updateClock() {
     minuteDial.style.transform = `rotate(${minuteAngle}deg)`;
     hourDial.style.transform = `rotate(${hourAngle}deg)`;
 
-    if(colorCount > 7) {
-        colorCount = 1;
+    if(colorCount > 23) {
+        colorCount = 0;
     }
-    timeZone.style.setProperty('--sora-color', soraColor[colorCount]);
+    timeZone.style.setProperty('--sora-color', soraColor[now.getHours()]);
+    console.log(now.getHours());
     colorCount++;
 }
 
-function checkOverFllow(type, current, deg) {
+function checkOverflow(type, current, deg) {
     switch(type) {
         case "second":
             if(current < deg) {
