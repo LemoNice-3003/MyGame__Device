4ff65b36b0fbde1e8176cce407e7683fca514a1e
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-12-25 00:23:46 +0900

    Update root.js
    12/25
    変更点
    ・クリアアイコンのチェックボックス部分のIDを登録
    ・ドキュメントコメントを書いてみた
    
    改善点
    これから少しずつドキュメントコメント増やしていきたい

--- root/root.js
+++ root/root.js
@@ -1,3 +1,6 @@
+/**
+ * @type {string} セッションストレージ内の、ログインの有無を判定しているflag
+ */
 const loginFlag = sessionStorage.getItem('loginFlag');
 
 const hamburgerMenu = document.getElementById('js-hamburger');
@@ -5,14 +8,32 @@ const hamburgerItems = document.getElementById('js-items');
 const container = document.getElementById('js-container');
 
 
+/**
+ * @type {Element | null} ログインしているユーザーの名前
+ */
 const nowUserName = document.querySelector('#nowUserName');
+/**
+ * @type {Element | null} ログインしているときにメニューに表示される要素（「ユーザー名」「Log out」）
+ */
 const loginNow = document.querySelector('#loginNow');
+/**
+ * @type {Element | null} ログインしていないときにメニューに表示される要素（「Log in」）
+ */
 const loginButton = document.querySelector('#loginButton');
 
 
-
+/**
+ * @type {string} セッションストレージ内の、ログインしているユーザーの名前
+ */
 const userName = sessionStorage.getItem('name');
+/**
+ * @type {number} ユーザーの名前の長さを格納するフィールド
+ */
 let textLength = 0;
+/**
+ * セッションストレージ内の、ログインしているユーザーの進捗度
+ * 
+ */
 const nowProgress = Number(sessionStorage.getItem('progress'));
 
 document.addEventListener('touchend', (event) => {
@@ -25,3 +46,11 @@ document.addEventListener('touchend', (event) => {
     
     lastTouchTime = currentTime;
 });
+
+
+const checkbox_1_0 = document.getElementById("checkbox_1_0");
+
+const checkbox_2_0 = document.getElementById("checkbox_2_0");
+const checkbox_2_1 = document.getElementById("checkbox_2_1");
+const checkbox_2_2 = document.getElementById("checkbox_2_2");
+const checkbox_2_3 = document.getElementById("checkbox_2_3");
