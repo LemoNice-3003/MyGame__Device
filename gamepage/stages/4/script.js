78b12495248e16c252f6f96e3149a8fefb91407b
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-03 16:12:56 +0900

    Implement loading and clearing progress functions
    1/3
    変更点
    内部ロジックを作った（最低限
    
    改善点
    スマホで動作確認してい定期

---
+++ gamepage/stages/4/script.js
@@ -0,0 +1,21 @@
+async function onload() {
+    if(nowProgress >= 5) {
+        await checkClearIcon(clearFlag_4, checkbox_4_0, "50vw", "50vh");
+    }
+}
+
+async function clearOnce() {
+    checkClearIcon(clearFlag_4, checkbox_4_0, "50vw", "50vh");
+    clearFlag_4 = true;
+    if (clearFlag_4 && nowProgress == 4) {
+        await sessionStorage.setItem('progress', nowProgress + 1);
+    }
+}
+
+window.addEventListener("beforeprint", clearOnce);
+
+// Safari対策
+const mq = window.matchMedia("print");
+mq.addEventListener("change", e => {
+    if (e.matches) clearOnce();
+});
