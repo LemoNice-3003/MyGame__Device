391182b428776428b5e9bd890354c74c936147ae
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-06 11:24:36 +0900

    Update script.js
    1/6
    変更点
    ・ホイールによるデフォルトの拡大・縮小機能をキャンセルし、こっちで新しく拡大・縮小機能を作ることにした（多分この方法のほうが管理が楽
    ・クリアアイコンに、拡大縮小機能で大きさを合わせたチェックボックスを当てはめていく感じのステージを予定している
    ・拡大・縮小機能はできてるから、あとやらなきゃいけないのは①決まった位置に当てはめたら拡大縮小と位置の移動をできないように固定、②光らせる + クリア判定の処理 の２つ
    
    改善点
    試験が思ったより早くに決まったからそっちを少し優先気味にしたい。そのためにもこれからの開発予定をこまめに確認していく必要がありそうだ

--- gamepage/stages/5/script.js
+++ gamepage/stages/5/script.js
@@ -1,14 +1,14 @@
-let parsent = document.getElementById('par');
-let vw = document.getElementById('vw');
-let px = document.getElementById('px');
+let _0_Width = checkbox_5_0.clientWidth;
+let _1_Width = checkbox_5_1.clientWidth;
+let _2_Width = checkbox_5_2.clientWidth;
+let _3_Width = checkbox_5_3.clientWidth;
+let _4_Width = checkbox_5_4.clientWidth;
 
-let parsentWidth = parsent.clientWidth;
-let vwWidth = vw.clientWidth;
-let pxWidth = px.clientWidth;
-
-let parsent_val = document.getElementById("par_val");
-let vw_val = document.getElementById('vw_val');
-let px_val = document.getElementById('px_val');
+let _0_val = document.getElementById('px_val_5_0');
+let _1_val = document.getElementById('px_val_5_1');
+let _2_val = document.getElementById('px_val_5_2');
+let _3_val = document.getElementById('px_val_5_3');
+let _4_val = document.getElementById('px_val_5_4');
 
 
 async function onload() {
@@ -17,6 +17,32 @@ async function onload() {
     }
 }
 
-parsent_val.textContent = parsentWidth + "px";
-vw_val.textContent = vwWidth + "px";
-px_val.textContent = pxWidth + "px";
+_0_val.textContent = _0_Width + "px";
+_1_val.textContent = _1_Width + "px";
+_2_val.textContent = _2_Width + "px";
+_3_val.textContent = _3_Width + "px";
+_4_val.textContent = _4_Width + "px";
+
+document.addEventListener('wheel', async function(event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+        
+        if (event.deltaY > 0) { //下方向（スクロールダウン）
+            _0_Width = Math.round(_0_Width / 1.5);
+            if(_0_Width < 50) {
+                _0_Width = Math.round(_0_Width * 1.5);
+                return;
+            }
+        }
+        else if (event.deltaY < 0) { //上方向（スクロールアップ）
+            _0_Width = Math.round(_0_Width * 1.5);
+            if(_0_Width > 1100) {
+                _0_Width = Math.round(_0_Width / 1.5);
+            }
+        }
+        let _0_NewWidth = _0_Width.toString() + "px"
+        await checkbox_5_0.style.setProperty("--defaultWidh-5-0", _0_NewWidth);
+        _0_val.textContent = _0_Width + "px";
+        await checkClearIcon(clearFlag_5, checkbox_5_0, "50vw", "50vh");
+    }
+}, { passive: false });
