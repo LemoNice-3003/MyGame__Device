7e6e2e6f613ba8019459efb65604562fc6e2d4bb
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-21 17:29:04 +0900

    Update script.js
    1/21
    変更点
    スマホ版に対応させる夢

--- gamepage/stages/5/script.js
+++ gamepage/stages/5/script.js
@@ -62,70 +62,92 @@ async function onload() {
 
 
 document.addEventListener('wheel', async function(event) {
-    if (event.ctrlKey) {
+    if(event.ctrlKey) {
         event.preventDefault();
         
-        if (event.deltaY > 0) { //下方向（スクロールダウン）
-            data.forEach(item => {
-                if(item.flag) {
-                    item.width = Math.round(item.width / 1.5);
-                }
-                if(item.width < 50) {
-                    item.width = Math.round(item.width * 1.5);
-                    return;
-                }
-            });
+        if(event.deltaY > 0) { //下方向（スクロールダウン）
+            zoomOut();
         }
-        else if (event.deltaY < 0) { //上方向（スクロールアップ）
-            data.forEach(item => {
-                if(item.flag) {
-                    item.width = Math.round(item.width * 1.5);
-                }
-                if(item.width > 1100) {
-                    item.width = Math.round(item.width / 1.5);
-                    return;
-                }
-            });
+        else if(event.deltaY < 0) { //上方向（スクロールアップ）
+            zoomIn();
         }
-
-
-        data.forEach(item => {
-            item.checkbox.style.setProperty(item.css, `${item.width}px`); //cssの値を変更（checkboxの大きさ
-        });
-
-        if(data[0].width <= 68 && data[0].flag) {
-            data[0].flag = false;
-            checkbox_5_0.style.backgroundColor = "rgb(157, 152, 135)";
-            await checkClearIcon(clearFlag_5, checkbox_5_0, "10vw", "80vh");
-        }
-        if(data[1].width == 113 && data[1].flag) {
-            data[1].flag = false;
-            checkbox_5_1.style.backgroundColor = "rgb(157, 152, 135)";
-            await checkClearIcon(clearFlag_5, checkbox_5_1, "10vw", "80vh");
-        }
-        if(data[2].width <= 50 && data[2].flag) {
-            data[2].flag = false;
-            checkbox_5_2.style.backgroundColor = "rgb(157, 152, 135)";
-            await checkClearIcon(clearFlag_5, checkbox_5_2, "50vw", "50vh");
-        }
-        if(data[3].width <= 60 && data[3].flag) {
-            data[3].flag = false;
-            checkbox_5_3.style.backgroundColor = "rgb(157, 152, 135)";
-            await checkClearIcon(clearFlag_5, checkbox_5_3, "70vw", "86vh");
-        }
-        if(data[4].width == 201 && data[4].flag) {
-            data[4].flag = false;
-            checkbox_5_4.style.backgroundColor = "rgb(157, 152, 135)";
-            await checkClearIcon(clearFlag_5, checkbox_5_4, "90vw", "1vh");
-        }
-
-        if(!data[0].flag && !data[1].flag && !data[2].flag && !data[3].flag && !data[4].flag) {
-            if(nowProgress == 5) {
-                clearFlag_5 = true;
-                await sessionStorage.setItem('progress', nowProgress + 1);
-            }
-        }
+        
+        transformBoxsize();
     }
 }, { passive: false });
+document.addEventListener('touchstart', async function(event) {
+    // 2本指でタッチされている場合のみ処理
+    if(event.touches.length === 2) {
+        if(isPinchZooming()) {
+            zoomIn();
+            await sleep(1500);
+        }
+    }
+}, false);
 
+function isPinchZooming() {
+  return window.visualViewport.scale > 1
+}
 
+function zoomIn() {
+    data.forEach(item => {
+        if(item.flag) {
+            item.width = Math.round(item.width * 1.5);
+        }
+        if(item.width > 1100) {
+            item.width = Math.round(item.width / 1.5);
+            return;
+        }
+    });
+}
+
+function zoomOut() {
+    data.forEach(item => {
+        if(item.flag) {
+            item.width = Math.round(item.width / 1.5);
+        }
+        if(item.width < 50) {
+            item.width = Math.round(item.width * 1.5);
+            return;
+        }
+    });
+}
+
+async function transformBoxsize() {
+    data.forEach(item => {
+        item.checkbox.style.setProperty(item.css, `${item.width}px`); //cssの値を変更（checkboxの大きさ
+    });
+    
+    if(data[0].width <= 68 && data[0].flag) {
+        data[0].flag = false;
+        checkbox_5_0.style.backgroundColor = "rgb(157, 152, 135)";
+        await checkClearIcon(clearFlag_5, checkbox_5_0, "10vw", "80vh");
+    }
+    if(data[1].width == 113 && data[1].flag) {
+        data[1].flag = false;
+        checkbox_5_1.style.backgroundColor = "rgb(157, 152, 135)";
+        await checkClearIcon(clearFlag_5, checkbox_5_1, "10vw", "80vh");
+    }
+    if(data[2].width <= 50 && data[2].flag) {
+        data[2].flag = false;
+        checkbox_5_2.style.backgroundColor = "rgb(157, 152, 135)";
+        await checkClearIcon(clearFlag_5, checkbox_5_2, "50vw", "50vh");
+    }
+    if(data[3].width <= 60 && data[3].flag) {
+        data[3].flag = false;
+        checkbox_5_3.style.backgroundColor = "rgb(157, 152, 135)";
+        await checkClearIcon(clearFlag_5, checkbox_5_3, "70vw", "86vh");
+    }
+    if(data[4].width == 201 && data[4].flag) {
+        data[4].flag = false;
+        checkbox_5_4.style.backgroundColor = "rgb(157, 152, 135)";
+        await checkClearIcon(clearFlag_5, checkbox_5_4, "90vw", "1vh");
+    }
+    
+    if(!data[0].flag && !data[1].flag && !data[2].flag && !data[3].flag && !data[4].flag) {
+        if(nowProgress == 5) {
+            clearFlag_5 = true;
+            await sessionStorage.setItem('progress', nowProgress + 1);
+        }
+    }
+}
