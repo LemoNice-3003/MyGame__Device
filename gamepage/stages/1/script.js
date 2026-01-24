1d18fb5a8c4a507fe6a57f31463faf74d485345e
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-25 02:58:46 +0900

    Update script.js

--- gamepage/stages/1/script.js
+++ gamepage/stages/1/script.js
@@ -17,6 +17,7 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', asy
     symbol.classList.toggle('active');
     await checkClearIcon(clearFlag_1, checkbox_1_0, "50vw", "calc(75vh + 40px)");
     clearFlag_1 = true;
+    await sessionStorage.setItem('clearFlag_1', true);
     if (clearFlag_1 && nowProgress == 1) {
         await sessionStorage.setItem('progress', nowProgress + 1);
     }
