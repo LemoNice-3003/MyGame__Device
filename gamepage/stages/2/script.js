5d3cd3ad0c95771290b91c61ab86a2a00bc13efe
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-25 02:58:03 +0900

    Update script.js

--- gamepage/stages/2/script.js
+++ gamepage/stages/2/script.js
@@ -3,7 +3,7 @@ const x = bounds.left + 45 + "px";
 const y = bounds.top + 45 + "px";
 
 async function onload() {
-    console.log(x, y);
+    //console.log(x, y);
     if(nowProgress >= 3) {
         await checkClearIcon(clearFlag_2, checkbox_2_0, x, y);
     }
@@ -15,19 +15,20 @@ function isAppLaunch() {
     );
 }
 
-console.log(isAppLaunch() ? 'PWA起動' : '通常ブラウザ');
+//console.log(isAppLaunch() ? 'PWA起動' : '通常ブラウザ');
 
 const isIOSStandalone = window.navigator.standalone === true;
 
 if(isIOSStandalone) {
-    alert('iOS ホーム画面から起動');
+    //alert('iOS ホーム画面から起動');
 }
 
 
 if(isAppLaunch() || isIOSStandalone) {
-    alert('clear');
+    //alert('clear');
     checkClearIcon(clearFlag_2, checkbox_2_0, x, y);
     clearFlag_2 = true;
+    await sessionStorage.setItem('clearFlag_2', true);
     if (clearFlag_2 && nowProgress == 2) {
         sessionStorage.setItem('progress', nowProgress + 1);
     }
