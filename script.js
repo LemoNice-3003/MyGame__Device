69c7d860d23e0c1c11b338142ec5597101d04a9c
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2026-01-25 02:56:29 +0900

    Update script.js
    1/25
    変更点
    新しいセーブデータを作成するときの処理を修正した

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ if ('serviceWorker' in navigator) {
 
 let lastTouchTime = 0;
 
-const newName = document.getElementById('newName');
 newName.form.addEventListener("keydown", (e) => {
     if(e.key === "Enter") {
         doneNewName();
@@ -20,13 +19,35 @@ newName.form.addEventListener("keydown", (e) => {
 });
 
 // 新しいセーブデータを作るときのボタンの処理
-function doneNewName() {
+async function doneNewName() {
+    const newName = document.getElementById('newName');
     // 異常処理
     if (!newName.value || newName.value.length > 10) {
         alert("名前が無効です。再入力してください。");
         return;
     }
 
+    await sessionStorage.setItem('loginFlag', true);
+    await sessionStorage.setItem('progress', 1);
+    await sessionStorage.setItem('name', newName.value);
+    await sessionStorage.setItem('clearFlag_1', false);
+    await sessionStorage.setItem('clearFlag_2', false);
+    await sessionStorage.setItem('clearFlag_3', false);
+    await sessionStorage.setItem('clearFlag_3_0', false);
+    await sessionStorage.setItem('clearFlag_3_1', false);
+    await sessionStorage.setItem('clearFlag_3_2', false);
+    await sessionStorage.setItem('clearFlag_3_3', false);
+    await sessionStorage.setItem('clearFlag_4', false);
+    await sessionStorage.setItem('clearFlag_5', false);
+    await sessionStorage.setItem('clearFlag_5_0', false);
+    await sessionStorage.setItem('clearFlag_5_1', false);
+    await sessionStorage.setItem('clearFlag_5_2', false);
+    await sessionStorage.setItem('clearFlag_5_3', false);
+    await sessionStorage.setItem('clearFlag_5_4', false);
+    await sessionStorage.setItem('clearFlag_6', false);
+    await sessionStorage.setItem('clearFlag_6_0', false);
+    await sessionStorage.setItem('clearFlag_6_1', false);
+    await sessionStorage.setItem('clearFlag_6_2', false);
     goToGamepage(newName.value, 1);
 }
 
@@ -108,7 +129,3 @@ $('#circle4')
     }).bind('touchend', function(){
         $(this).removeClass('hover');
 });
-
-// $(function(){
-//    $("span").css({"color" : "red", "font-size" : "100px", "border" : "solid 5px"});
-// });
